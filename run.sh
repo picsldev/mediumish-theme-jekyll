5c733de806d7a2ba769feb66135fafd5a44cec48
Luis Cabrera Sauco <icorrecam@gmail.com>
2019-08-13 14:23:54 +0100

    Añadido algnos comandos más ...

--- run.sh
+++ run.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env sh
 
+
+# installs any new/updated gems and dependencies - but if they are already installed, nothing is done
+bundle install
+
+# runs through your installed gems, and grabs the newest, allowed versions, as defined in your Gemfile
+bundle update
+
+# bypasses bundler, and installs the gem at the system level (i.e. outside your application's code bundle)
+#gem install my_gem.gem
+
+# bypasses bundler, and updates the gem at the system level (i.e. outside your application's code bundle)
+#gem update my_gem.gem
+
+
+
+
 # update Rubygems
-gem update --system
+# gem update --system
+gem update
 
 # update bundler
 gem install bundler
